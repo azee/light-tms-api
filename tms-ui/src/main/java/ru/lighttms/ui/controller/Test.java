060493da0d94aa725d3c0b57b9045d2319772490
azee <azee@yandex-team.ru>
2012-11-18 05:22:36 +0400

    Adding a wellcome form

--- tms-ui/src/main/java/ru/lighttms/ui/controller/Test.java
+++ tms-ui/src/main/java/ru/lighttms/ui/controller/Test.java
@@ -3,6 +3,7 @@ package ru.lighttms.ui.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -15,11 +16,10 @@ import org.springframework.web.servlet.ModelAndView;
 public class Test {
 
     @RequestMapping(method = RequestMethod.GET)
-
-    public ModelAndView testAction(){
+    public ModelAndView testAction(@RequestParam("name") String name){
 
 		ModelAndView model = new ModelAndView("TestPage");
-		model.addObject("msg", "hello world");
+		model.addObject("msg", name);
 
 		return model;
 	}
